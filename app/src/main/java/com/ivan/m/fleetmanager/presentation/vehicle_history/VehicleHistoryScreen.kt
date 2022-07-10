02394dd11d1ace4590791f22a85919d4ddf8fb5b
ivancity <shieldm@hotmail.com>
2022-07-10 13:12:03 -0500

    adding polylines and handling loading states and error states

--- app/src/main/java/com/ivan/m/fleetmanager/presentation/vehicle_history/VehicleHistoryScreen.kt
+++ app/src/main/java/com/ivan/m/fleetmanager/presentation/vehicle_history/VehicleHistoryScreen.kt
@@ -1,19 +1,24 @@
 package com.ivan.m.fleetmanager.presentation.vehicle_history
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.RoundCap
+import com.google.maps.android.SphericalUtil
 import com.google.maps.android.compose.*
-import com.ivan.m.fleetmanager.domain.model.LatLong
 import com.ivan.m.fleetmanager.presentation.components.AppBarState
 import com.ivan.m.fleetmanager.presentation.vehicle_history.components.DatePickerComponent
 
@@ -56,32 +61,56 @@ fun VehicleHistoryBody(
     }
 }
 
+
 @Composable
 fun MapView(state: VehicleHistoryState) {
     val centerMarker = state.firstCoordinate?.let {  LatLng(it.latitude, it.longitude) }
-    if (centerMarker != null) {
-        val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(centerMarker, 11.5f)
+    val googleCoordinates = state.latLongList.map { LatLng(it.latitude, it.longitude) }
+//    val length = SphericalUtil.computeLength(googleCoordinates)
+
+
+    Box(Modifier.fillMaxSize()) {
+        if (state.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
         }
-        Box(Modifier.fillMaxSize()) {
+        if (state.error.isNotEmpty()) {
+            Text(
+                text = "Something went wrong",
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+        if (centerMarker != null) {
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(centerMarker, 11.5f)
+            }
             GoogleMap(
                 modifier = Modifier.fillMaxSize(),
                 cameraPositionState = cameraPositionState
             ) {
-                Polyline(points = state.latLongList.map { LatLng(it.latitude, it.longitude) })
+                Polyline(
+                    points = googleCoordinates
+                )
+                val startMarker = LatLng(state.firstCoordinate.latitude, state.firstCoordinate.longitude)
                 Marker(
                     state = MarkerState(
-                        position = LatLng(state.firstCoordinate.latitude, state.firstCoordinate.longitude)
+                        position = startMarker
                     ),
                     title = "Start"
                 )
                 if (state.lastCoordinate != null) {
+                    val endMarker = LatLng(state.lastCoordinate.latitude, state.lastCoordinate.longitude)
                     Marker(
                         state = MarkerState(
-                            position = LatLng(state.lastCoordinate.latitude, state.lastCoordinate.longitude)
+                            position = endMarker
                         ),
                         title = "End"
                     )
+                    val bounds = LatLngBounds.Builder()
+                    bounds.include(startMarker)
+                    bounds.include(endMarker)
+                    cameraPositionState.move(CameraUpdateFactory.newLatLngBounds(bounds.build(), 200))
                 }
             }
             Text(
@@ -90,6 +119,7 @@ fun MapView(state: VehicleHistoryState) {
                     .align(Alignment.BottomCenter)
                     .padding(bottom = 12.dp)
             )
+
         }
     }
 }
@@ -98,6 +128,8 @@ fun MapView(state: VehicleHistoryState) {
 @Composable
 fun VehicleHistoryScreenPreview() {
     VehicleHistoryBody(
-        state = VehicleHistoryState()
+        state = VehicleHistoryState(
+            isLoading = true
+        )
     ) { _ -> }
 }
